8eea9470c3ce7644f93fe417949ecd5bca0f3dbc
Nix Updater <actions@github.com>
2022-05-09 09:13:41 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,19 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2022-05-07T16:36:40Z";
+  timestamp = "2022-05-08T23:06:08Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-3497cdca9acb586d24cd49a4ac7eed28301bdaae/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "070nabwvbn6i1phqf3nk9c4czmsb4hi5vhihgbbg0287dzx6nghp";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-85f69d950505e92d2047f38c3ca69281fc0e000b/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "0y0fczpyp86rvnfdx371q23wfyz090fica9sx1gr8afpf9dzz56j";
     };
+    "aarch64-linux" = {
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-85f69d950505e92d2047f38c3ca69281fc0e000b/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "13kvhnbyr2sjww7rccv13j030x5kdiapjrwm39dx87ll157ihs3i";
+    }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-3497cdca9acb586d24cd49a4ac7eed28301bdaae/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "01a7gm2dx9za9ni6im5bqc77vfhwjnc9iwm863n1vcdwhwqvgn6w";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-85f69d950505e92d2047f38c3ca69281fc0e000b/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "0zmasxymw76agzjsjy7lw75qscnwrwcraxip0sfpyqfsgpa18d7h";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-3497cdca9acb586d24cd49a4ac7eed28301bdaae/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "0f3h746xlp2dmy4y6lcs67dibhjr0pw479ibcfa2y1hkkyidrhrz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-85f69d950505e92d2047f38c3ca69281fc0e000b/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "039ngshidls71fa432q91pk3wzs60854y150sngdz59s7p3caib8";
     };
   };
 }
