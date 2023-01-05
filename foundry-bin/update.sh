1666af93ca3b6b37d975e4b20cfded8fdb2f301f
Andrey Petrov <andrey.petrov@shazow.net>
2023-01-05 11:38:12 -0500

    foundry-bin/update.sh: Use .created_at for monthly, same as upstream prune script

--- foundry-bin/update.sh
+++ foundry-bin/update.sh
@@ -7,8 +7,8 @@ set -euxo pipefail
 GITHUB_API_URL="https://api.github.com/repos/foundry-rs/foundry/releases"
 
 SCHEDULE=${1:-}
-if [ "$SCHEDULE" == "monthly" ];then
-    release_filter="map(select(.name | match(\"-01-01\"))) |"
+if [[ "$SCHEDULE" == "monthly" ]];then
+    release_filter="map(select(.created_at | match(\"-01-01\"))) |"
     echo "Using monthly release filter."
 else
     release_filter=""
