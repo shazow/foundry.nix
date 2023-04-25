929c32ba88864806020c08458e4f963610f10863
Nix Updater <actions@github.com>
2023-04-25 09:09:39 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -5,11 +5,11 @@
   sources = {
     "x86_64-linux" = {
       url = "https://github.com/foundry-rs/foundry/releases/download/nightly-dedc31eed0683764ae542b9b575c21f6bd2a5c60/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "044il90vlxdb38wvf67q9lyb1f15wdmrnardn2ji8vwwlx745c6a";
+      sha256 = "1d38pn8bk0ims14hkdchkx5nb1kx1gnqxx3nvqbm0c04ky2m5fn0";
     };
     "aarch64-linux" = {
       url = "https://github.com/foundry-rs/foundry/releases/download/nightly-dedc31eed0683764ae542b9b575c21f6bd2a5c60/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "18al8i978jy5z6lw23ps7zkflynwjdwvx01xa9s4i477m0drzsg5";
+      sha256 = "0rkbqv0n2ly2lcavcr2qbn3digzyj6ldj5ync5581149kzk415lg";
     }; 
     "x86_64-darwin" = {
       url = "https://github.com/foundry-rs/foundry/releases/download/nightly-dedc31eed0683764ae542b9b575c21f6bd2a5c60/foundry_nightly_darwin_amd64.tar.gz";
