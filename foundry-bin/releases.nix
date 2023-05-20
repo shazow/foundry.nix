223e693a484fcb6737dae5e895d930da2f614986
Nix Updater <actions@github.com>
2023-05-20 09:09:02 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2023-05-18T00:01:21Z";
+  timestamp = "2023-05-19T12:16:10Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-a5d506ee26b756e476b9bac167890377dc0e6bff/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "08j1g4d7967a675vj6y86cdn5g8nb8rkivmcqllqpwl2rka4j3jc";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-c331b5eeee1b4151ef7354a081667e2d770b37f5/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-a5d506ee26b756e476b9bac167890377dc0e6bff/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "1xkhmbynflxzkp4vc0nspq1zfmqj6mxdar19cbyl3qn9bhkkli64";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-c331b5eeee1b4151ef7354a081667e2d770b37f5/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-a5d506ee26b756e476b9bac167890377dc0e6bff/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "0safkq2x185zwv0d3131a3bnhsjhhqphq2z9nbihb1jml8d2qfv9";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-c331b5eeee1b4151ef7354a081667e2d770b37f5/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-a5d506ee26b756e476b9bac167890377dc0e6bff/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "0xvw96f0q5vmrl2axv3dz1ii56bv1zmp46yd4807yxbwjlfdzjlc";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-c331b5eeee1b4151ef7354a081667e2d770b37f5/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "";
     };
   };
 }
