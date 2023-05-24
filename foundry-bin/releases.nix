1b6fec21df998346b4f775a9427ea9fe8beb235f
Nix Updater <actions@github.com>
2023-05-24 09:09:19 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2023-05-22T15:58:55Z";
+  timestamp = "2023-05-23T09:55:33Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-4f9369f876c70cefe53c9fea5919e078b50e29ae/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-638bd2e0040962731d220eb33d8b692ca99afaa5/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "0v3dgqc2jvykn56i1ci8f2vqa4n8fwhbwspwcvxjd931jwy1ckdz";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-4f9369f876c70cefe53c9fea5919e078b50e29ae/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-638bd2e0040962731d220eb33d8b692ca99afaa5/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "1m3m6xa8f94pygyccjlwhjs3afjjvj1hsdb6lysg7f7rd6kpv4l1";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-4f9369f876c70cefe53c9fea5919e078b50e29ae/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-638bd2e0040962731d220eb33d8b692ca99afaa5/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "0hnybp2g6agsrc2c89vrsrlscx4j0w1591w9y5v531dd5hiafrll";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-4f9369f876c70cefe53c9fea5919e078b50e29ae/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-638bd2e0040962731d220eb33d8b692ca99afaa5/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "1s0zvjwy47j07hcw3b6w17daq7c3rvfv7yvnn7pfs4xvqb89wq3z";
     };
   };
 }
