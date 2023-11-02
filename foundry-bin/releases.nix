242107a320167191039d091a8c4522cfb6ef9988
Nix Updater <actions@github.com>
2023-11-02 09:09:25 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2023-10-31T19:52:27Z";
+  timestamp = "2023-11-01T10:03:09Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-799b82071cca5f58e1ad3df0bfb1f920ff78407d/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "1dpsiap6v67l2dralgv7rxpapf7m8dp8p80xdzg3k9v17nj9p4bj";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-60ec00296f00754bc21ed68fd05ab6b54b50e024/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "03labb6sl714cgwi921smj594grkh3q5f42i8jc74r5xn19cq8d6";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-799b82071cca5f58e1ad3df0bfb1f920ff78407d/foundry_nightly_linux_arm64.tar.gz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-60ec00296f00754bc21ed68fd05ab6b54b50e024/foundry_nightly_linux_arm64.tar.gz";
       sha256 = "";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-799b82071cca5f58e1ad3df0bfb1f920ff78407d/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-60ec00296f00754bc21ed68fd05ab6b54b50e024/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "09m9wsk007p93nw59k9m91ymypfgln4navyrndj4hf98v6f281k6";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-799b82071cca5f58e1ad3df0bfb1f920ff78407d/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-60ec00296f00754bc21ed68fd05ab6b54b50e024/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "0g807qsawszb8dzka4rspd0dcsjy7kqkgcyggvvilj46mvx8vgz6";
     };
   };
 }
