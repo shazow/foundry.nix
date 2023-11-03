02e82154aeff5d8b73cae1bc712c1e26b72024d6
Nix Updater <actions@github.com>
2023-11-03 09:09:29 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2023-11-01T10:03:09Z";
+  timestamp = "2023-11-02T21:55:01Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-60ec00296f00754bc21ed68fd05ab6b54b50e024/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "03labb6sl714cgwi921smj594grkh3q5f42i8jc74r5xn19cq8d6";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-5dda5b1daeab73f9f87589b1944877127c48e425/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-60ec00296f00754bc21ed68fd05ab6b54b50e024/foundry_nightly_linux_arm64.tar.gz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-5dda5b1daeab73f9f87589b1944877127c48e425/foundry_nightly_linux_arm64.tar.gz";
       sha256 = "";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-60ec00296f00754bc21ed68fd05ab6b54b50e024/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "09m9wsk007p93nw59k9m91ymypfgln4navyrndj4hf98v6f281k6";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-5dda5b1daeab73f9f87589b1944877127c48e425/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-60ec00296f00754bc21ed68fd05ab6b54b50e024/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "0g807qsawszb8dzka4rspd0dcsjy7kqkgcyggvvilj46mvx8vgz6";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-5dda5b1daeab73f9f87589b1944877127c48e425/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "1fl8sjc398cvja8lyyjchn04myl5r9ik378n70h641c5197y84vw";
     };
   };
 }
