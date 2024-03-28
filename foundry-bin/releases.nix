81ebef79670056b6f756f92fa505da06ce242fba
Nix Updater <actions@github.com>
2024-03-28 09:09:52 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2024-03-26T16:52:12Z";
+  timestamp = "2024-03-27T21:33:09Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-9148dbc2fe0c72e249669b11d04caae593fa6113/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "1m829bi7pazr56xsd7l7b1gmnbm0bpjdpacbwhiwwvv4crcfyb9s";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "0sf5kz2cqm12041zcq2x8z6zkmzzrwdra6bk9ix048hac0cx8qpr";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-9148dbc2fe0c72e249669b11d04caae593fa6113/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "02wh1ly0mrdd5sakxf4861gyavnn80s754ikh0zq2zp5xq25gsyz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "0h3fzvfm6gyah5w72ygxw5s16dvwbcz0wybsfan1z0nji22565rz";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-9148dbc2fe0c72e249669b11d04caae593fa6113/foundry_nightly_darwin_amd64.tar.gz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz";
       sha256 = "1m41idj5vbam708n9vzbg0d3iix237b9m0p17pp8g0jvlbh5ijlp";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-9148dbc2fe0c72e249669b11d04caae593fa6113/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "160h9jnhn80zpp14v0sfhicj6a4506yr1cmm0aqyz5s12k82f2ql";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "1a7h5vqnknmzfal418dx4sasx0d8n0yjfhy60x19xxwwfmj63agz";
     };
   };
 }
