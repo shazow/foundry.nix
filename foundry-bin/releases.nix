f343ac2c125e5361cb99fcf8fb5050a7ada93489
Nix Updater <actions@github.com>
2024-04-10 09:10:01 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,22 +1,22 @@
 {
   version = "0.0.0";
-  timestamp = "2024-04-08T22:19:16Z";
+  timestamp = "2024-04-09T22:38:34Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-14daacfe40565b19bdfc3b78c6cf1775f54529da/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "1qcaiypk77w5zbhzgix94rx440p3c5xn6x22i27n4m8drm529j1j";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "1w2a4448dg9f45icjmv49s1mh1infyarsyh5cgm6iamgkq0spk9k";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-14daacfe40565b19bdfc3b78c6cf1775f54529da/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "15666lj2njcjz8z0cjrgqr5c5f75xnnwnmf34h1dkh060klss7r9";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "041kf25qfnp8fgvl7dpbj0jvg69blzg053vdman2pqxcwcps4ihv";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-14daacfe40565b19bdfc3b78c6cf1775f54529da/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "1b7rmlyappsykmxb6j1sfr503sr7z8518hbn08bqwlinz5x4jsig";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "1hfs8dzrdq07xqsvmh49kvqmc387k42vbyfq8sh3gp31wr0wprbh";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-14daacfe40565b19bdfc3b78c6cf1775f54529da/foundry_nightly_darwin_arm64.tar.gz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_arm64.tar.gz";
       sha256 = "0blcj4vcgavp9sbsv9g6899q2p850xnr8h419hx3vl9l8891i2xv";
     };
   };
