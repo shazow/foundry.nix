3aac42c3e6f46d852942516e397ee745c5b2be98
Nix Updater <actions@github.com>
2024-11-11 09:10:16 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2024-11-09T17:04:49Z";
+  timestamp = "2024-11-10T11:19:11Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-896794a21aef9326b3350ae243fb6d91ea945d65/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "12qdyif7q53znj6pa1bp9s3h9iv267pynhsg7dsd78iks82nqg4m";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "1h78m2c1sg7gnq2c5bhkq1fy52rl1sg5rwm5qvvkk6zipg1gzx23";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-896794a21aef9326b3350ae243fb6d91ea945d65/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "1clc3l1y74b55w3kkq750n1r85zx57b86xalbnc5zk2ggi7vhjl2";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "1xvw3mk32jriq9n7cmgl7drkfdzv091adq8d1jrkvi8bcv0gcgy3";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-896794a21aef9326b3350ae243fb6d91ea945d65/foundry_nightly_darwin_amd64.tar.gz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz";
       sha256 = "1irnhvjgs209ckp54yzmhihfbyvb9jyddhwxl9fkd63zhvp3nh92";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-896794a21aef9326b3350ae243fb6d91ea945d65/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "1yqbpch3k4rspjqr6p42cv4s6kr51qm4605f5nkcv5fy7hvaz1g9";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "0jpkccy5rwiwgxwg9qjyaa6dan7svwysgsyzn9p0nqqrdfdg2pch";
     };
   };
 }
