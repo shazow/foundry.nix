ea840fe392dc5e156a4b707714d0208f610b18c7
Nix Updater <actions@github.com>
2025-01-27 15:54:59 +0000

    foundry-bin/releases.nix: Stable update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2025-01-23T16:49:42Z";
+  timestamp = "2024-12-19T16:59:08Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-36d1a1ba7391957770638d2bbacd669adb5cfc00/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "1xphrmflr1rq992ksyanfy73sb5iypfq3plalqjg1vnbjyyidxlc";
+      url = "https://github.com/foundry-rs/foundry/releases/download/stable/foundry_stable_linux_amd64.tar.gz";
+      sha256 = "0x6yazj9jh04lvaz2n76zahdx5a5scvrsm1figj800hvrwa1pn7k";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-36d1a1ba7391957770638d2bbacd669adb5cfc00/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "1f71q4j5svq7z6rg8zacdzjq7vxy47gblciz78v84gshc6848ahs";
+      url = "https://github.com/foundry-rs/foundry/releases/download/stable/foundry_stable_linux_arm64.tar.gz";
+      sha256 = "0vbg1a3rs2r8f8vk136lyks7xw2ap9r8wf81nryvla76jqbsszrk";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-36d1a1ba7391957770638d2bbacd669adb5cfc00/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "0vp5kg4y4238p4h4rv5jygyamb3nv2221ay4rggcx84m8pg6kija";
+      url = "https://github.com/foundry-rs/foundry/releases/download/stable/foundry_stable_darwin_amd64.tar.gz";
+      sha256 = "1w5p7zfgv4a41bsr51i4mvinb6wh0r8sr8q31bl090nrfvs15gd8";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-36d1a1ba7391957770638d2bbacd669adb5cfc00/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "1rpgdhggf2q73ajr8hc2vldc6yfn8ja4xdpjkgqbp0q3rvj30hbz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/stable/foundry_stable_darwin_arm64.tar.gz";
+      sha256 = "0n3hbhiq9nbhdxclqdnvcv22zk064xvn7qix98s9dgzac85wb52r";
     };
   };
 }
