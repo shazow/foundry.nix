9e9fee3063894a2b561d52812a25fa54c1e7c644
Nix Updater <actions@github.com>
2025-02-25 09:10:14 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2025-02-23T19:10:05Z";
+  timestamp = "2025-02-24T14:35:04Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-9066359bc017179c15e80f866369d7c9d6c060e3/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "189q5flm7k5liqs95wjj41dqmj1lrbjjq84ixixxljkpdridpcdk";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "06x04b2m2qk4vkc09q4n6czjw1svrhika7vricsjd0g768bp3lz3";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-9066359bc017179c15e80f866369d7c9d6c060e3/foundry_nightly_linux_arm64.tar.gz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_arm64.tar.gz";
       sha256 = "1pgg5z59fknibx7pdc277lq58bmn83qg85cbqdkyyzpah1wvg1yy";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-9066359bc017179c15e80f866369d7c9d6c060e3/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "1284dakw79x0gq72wcl2i4ka5x0dyak6rc93r8ayl4askxm8hvpj";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "1flki2awqb6ppsw4bscm4a4dqff3rg5981plfp6kyzhdpqv5c5gw";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-9066359bc017179c15e80f866369d7c9d6c060e3/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "14i3qkx44pjca06s5x4anadlvxd2a69sbahz0jr5cv5k12y1ijrk";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "15d0j8hx3gbvxzsdirjxam5z2a29l57sn4dcbkymbplbahva165r";
     };
   };
 }
