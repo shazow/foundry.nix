208d6e485285e0c5a9690e69094f34ff24560620
Nix Updater <actions@github.com>
2025-05-25 09:09:57 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2025-05-23T22:44:11Z";
+  timestamp = "2025-05-24T14:01:32Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-b8118232de784e6bb650b95cbdc3b65172c80494/foundry_nightly_linux_amd64.tar.gz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz";
       sha256 = "14zmgq1imlvdpqsr7w6jmvfi2kdfvwv6agim8gkcg1rsl80g944z";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-b8118232de784e6bb650b95cbdc3b65172c80494/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "0yikrrqq0ym66619n6hqvc1dw6kf4abyfiz16sajkb5m9vfpyvwm";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "1rbr3hwsn8q51n2jnbk7zsrsyv6qdqg0j15xvm3kj3n94s1fmzhj";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-b8118232de784e6bb650b95cbdc3b65172c80494/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "0lgz324vpkvd9mp40s2ib7jwzcadhkj9c78acbm2p68gzhkwjc2v";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "177kiiz1lcal0h13rh1nlc6fzkag3n93i6vs200h1lr0y2lf8lrd";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-b8118232de784e6bb650b95cbdc3b65172c80494/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "0bcqqjrvjw82awrswcf52dgkf5vd9nr0cxirxil5pxf97xclrfpk";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "0xavj5lzm30xz6ah1qaajpm9j3mcxi4mly415fn0alfmvkxnzhsz";
     };
   };
 }
