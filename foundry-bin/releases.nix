5af12b6f2b708858ef3120041546ed6b038474a5
Nix Updater <actions@github.com>
2025-06-09 09:10:37 +0000

    foundry-bin/releases.nix: Stable update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
-  version = "1.2.2";
-  timestamp = "2025-05-30T12:08:24Z";
+  version = "1.2.3";
+  timestamp = "2025-06-05T16:51:14Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/v1.2.2/foundry_v1.2.2_linux_amd64.tar.gz";
-      sha256 = "10pxqnswx7326mz87948ah3bp6b4dwfxxy163ar14xhjv4j0hfmm";
+      url = "https://github.com/foundry-rs/foundry/releases/download/v1.2.3/foundry_v1.2.3_linux_amd64.tar.gz";
+      sha256 = "0b7rjnap63mmzcigp6mb082z9m6rh0wrv2f7yvrkk5m6hmfbg1rr";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/v1.2.2/foundry_v1.2.2_linux_arm64.tar.gz";
-      sha256 = "0sgfp0lvdhzr27f6gm6i1ipfhif6nljslwqfknlqf0wpcivr0rqx";
+      url = "https://github.com/foundry-rs/foundry/releases/download/v1.2.3/foundry_v1.2.3_linux_arm64.tar.gz";
+      sha256 = "0ngyryqhgpy391rpdv5agm2rgpf21y38yyld39fsapbhzzbz9nia";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/v1.2.2/foundry_v1.2.2_darwin_amd64.tar.gz";
-      sha256 = "1sbcjwgxiq884frn94cn5l7sld6abvmr23b2h29pi8k8c67cp9d5";
+      url = "https://github.com/foundry-rs/foundry/releases/download/v1.2.3/foundry_v1.2.3_darwin_amd64.tar.gz";
+      sha256 = "0m1dx2s5xkkp6xj412jf6vnq2vx2bavi8hr6nkgbizf9drwhbv8v";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/v1.2.2/foundry_v1.2.2_darwin_arm64.tar.gz";
-      sha256 = "1f1nh170nlgy28vvqr4ak4pg6bb9c44608yyk800vbpijdlmnyx5";
+      url = "https://github.com/foundry-rs/foundry/releases/download/v1.2.3/foundry_v1.2.3_darwin_arm64.tar.gz";
+      sha256 = "0cq0fbr8qisv1wmkngshszmr1h1bfmyvn9kl5zpxivwmnhjy7c1q";
     };
   };
 }
