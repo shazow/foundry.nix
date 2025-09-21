48071fb24ee21fc160604473c7e4045ff5871d18
Nix Updater <actions@github.com>
2025-09-21 09:09:57 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2025-09-19T11:56:12Z";
+  timestamp = "2025-09-20T18:58:35Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-f7563cf44840b7df465b11a52714ed5d89d5830a/foundry_nightly_linux_amd64.tar.gz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz";
       sha256 = "1y7ll2hmwnb282s31ixya1wk31kps76ikcdvqwa2ck47wrgb3hy4";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-f7563cf44840b7df465b11a52714ed5d89d5830a/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "06a3684ah5q5gfghbqnjd26hqapn3zzsmwchvggflzn337ribq9l";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "1ppn1avkmwrb2a0960mwbirki7zzisg5cjm6f76jmkpnglcmc7h8";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-f7563cf44840b7df465b11a52714ed5d89d5830a/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "1a8ppmj2ns7z3iqmar42yl2s8qgfxfczfsd88z31493n1byaqzsl";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "1vgkn8w21irwngd0h2gbz7spnfsscn4qkizlv9hhxsgcnhh8kgpn";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-f7563cf44840b7df465b11a52714ed5d89d5830a/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "1i56v93ma3h1fwahd75vmjki4s8px6r8r09h3yi8ms2yxj3srm3f";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "0b8cc8f40jyd7ygbs8m4m9bj33gyklhb9d1jj5ig0gwfahxzi9pm";
     };
   };
 }
