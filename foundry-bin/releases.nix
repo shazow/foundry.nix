e0d18d31307f80656a88938e3b2896e0042a662a
Nix Updater <actions@github.com>
2025-10-23 07:03:54 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2025-10-21T18:25:07Z";
+  timestamp = "2025-10-23T05:12:37Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-e98bff5a57d9f23e60b96e23627512eb5397a598/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "1250h77bdjllq7jvqw3j976raqfmxdahvry1l14d1wwyrgn39r6y";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "14g3gfyg9lz4g92xinj8dk82nc2gm1nybrg3rb5hf64j5ps6ik24";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-e98bff5a57d9f23e60b96e23627512eb5397a598/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "02fkrhk13dsb961cb9471qh87y9p9z1yq8fcz450kfxl3lirwdhq";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "1hyggfrx2h8r47rx6v7hcakxs48zqva3rmswkf923g8c3wmma0n0";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-e98bff5a57d9f23e60b96e23627512eb5397a598/foundry_nightly_darwin_amd64.tar.gz";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz";
       sha256 = "158ckw7894d4wybqp6i8qib21akvvljarp3y1c8iyfbc3vsqi1wa";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-e98bff5a57d9f23e60b96e23627512eb5397a598/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "0wjcynf7v7ap7zq9fbq2hzlk7qhvac6yannqfpy0g23w2akhz8v2";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "10mppvb5rfr11cylxcj4hkc126p1lghaid8vjdfvr955wdcd0bbz";
     };
   };
 }
