c469ee69247a8ff523d5684ddc5000fcb7507bf4
Nix Updater <actions@github.com>
2025-10-30 07:03:35 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2025-10-29T05:13:08Z";
+  timestamp = "2025-10-29T10:31:49Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "02gq6jq7bc776rfdajw1c3bxkvhsyg9nsfg93pnh30yf3bi8j5a9";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-b70e1f938612a31684cc22c94abe78dfecb03566/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "0ki6zcil71b1ivmanzb6y5wa307z7p2z3jymfa66j47h22aa0w4w";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "1cgm5y9dmcyjfdsiwmfr6wvls3kyi8vk26hfbcx2vgmr7cv7xs7a";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-b70e1f938612a31684cc22c94abe78dfecb03566/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "0x5fhfga8qrv12i58k6i8srp5xh98wladw9dmpczyxk9jpvgf59w";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "1q5a9zlf9zrcffqdn0li1hrlmq85kn21wjf8q3rpnvbfksq5i9j8";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-b70e1f938612a31684cc22c94abe78dfecb03566/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "0xlhyq0diaiz063b2mkw93rqyc9bsxcs6n2hrr7r3cqxrr54cw28";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "1xrfjm9j6f96bbnsjj1bzjlji67zq2dv35kv1ijj6p1cv8w5p5nd";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-b70e1f938612a31684cc22c94abe78dfecb03566/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "07x23axncl4qmn1shlhbjr0pgkl50xm5vhwxfppa7mzvvvbrq46v";
     };
   };
 }
