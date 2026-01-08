db8164f65f0a3ae3bf077353f8dc0c92e67d886b
Nix Updater <actions@github.com>
2026-01-08 07:06:13 +0000

    foundry-bin/releases.nix: Nightly update

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -5,11 +5,11 @@
   sources = {
     "x86_64-linux" = {
       url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "1mgphk1bmygqskf6vjyh4kp6f7nrv7c5vgf12vkgrxjj4v80a464";
+      sha256 = "1vra33gx34zjgapnwkpncskwar1yfdhv0yrvasijwlq3x5sgrsf5";
     };
     "aarch64-linux" = {
       url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "1v2lsza2nvxq5biky9irh731sxa59z54sclfx0lgkj4x35z1hls1";
+      sha256 = "0g9zac2rh1bbk4sr93l6zqpqx1kc2grdyz3gmv3hw7y96pmbnl4b";
     }; 
     "x86_64-darwin" = {
       url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz";
