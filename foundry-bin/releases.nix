db83c1675648088f4141c714389f25868ebfa57a
Nix Updater <actions@github.com>
2026-03-01 09:14:32 +0000

    foundry-bin/releases.nix: Nightly update for monthly branch

--- foundry-bin/releases.nix
+++ foundry-bin/releases.nix
@@ -1,23 +1,23 @@
 {
   version = "0.0.0";
-  timestamp = "2026-02-01T23:45:57Z";
+  timestamp = "2026-03-01T00:38:31Z";
 
   sources = {
     "x86_64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-33b34ec5eb9465226df7e1f094b7c0041381ab1b/foundry_nightly_linux_amd64.tar.gz";
-      sha256 = "1h57g9mszy8n5q7cfwgh7yk0qw0686qynm1ls1c6nwcqx7w9ss44";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_amd64.tar.gz";
+      sha256 = "1x48sn7rg8v7w6yr921nimcf92vg34fkm5v56m8vikjlpjphkvs9";
     };
     "aarch64-linux" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-33b34ec5eb9465226df7e1f094b7c0041381ab1b/foundry_nightly_linux_arm64.tar.gz";
-      sha256 = "0a6f6n3l9rynhcj41v2q4ll7ld8rla1rkynnbfigkq6d62vns814";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_linux_arm64.tar.gz";
+      sha256 = "113375kv2a2s1vpna73wg3p1ann39rcda6wra3cqsgyq5vh8i699";
     }; 
     "x86_64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-33b34ec5eb9465226df7e1f094b7c0041381ab1b/foundry_nightly_darwin_amd64.tar.gz";
-      sha256 = "1g2c1i4bi7ilmwnbcl3kly241gkp91hiklkrgamc4d70cfkj799j";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_amd64.tar.gz";
+      sha256 = "06b7cb5mh0insndzpda8ch8d7i82d5r376fybmdfm9xdsafaj0ii";
     };
     "aarch64-darwin" = {
-      url = "https://github.com/foundry-rs/foundry/releases/download/nightly-33b34ec5eb9465226df7e1f094b7c0041381ab1b/foundry_nightly_darwin_arm64.tar.gz";
-      sha256 = "0s5x2d3fs7vk0gw2aw2hdxvwc9r4kh7hrizv0a6dys87lkvc2c6k";
+      url = "https://github.com/foundry-rs/foundry/releases/download/nightly/foundry_nightly_darwin_arm64.tar.gz";
+      sha256 = "0z5ixhdhb8c0n622y41by8mw986wal68x6a9gsfvqmcb3kxm2mvc";
     };
   };
 }
